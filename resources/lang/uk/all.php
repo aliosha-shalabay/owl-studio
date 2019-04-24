78e2e5aacb60918699b1e7b4a1704793446168f4
Aliosha <aliosha.shalabay@gmail.com>
2019-04-24 12:10:40 +0300

    'lang'

---
+++ resources/lang/uk/all.php
@@ -0,0 +1,81 @@
+<?php
+
+return [
+
+    'home' => 'Головна',
+    'services' => 'Послуги',
+    'about' => 'Про нас',
+    'portfolio' => 'Портфоліо',
+    'pricing' => 'Ціни',
+    'contact' => 'Контакти',
+    //------------------------------------------------------------------------------------------------------------------
+
+    'main_slider_1' => 'Ми - розробники.',
+    'main_slider_2' => 'Ми - дизайнери',
+    'main_slider_3' => 'Ми - маркетологи',
+    'main_slider_sub_1' => 'I\'m Creative I\'m Freelancer.I\'m Develop',
+    'main_slider_sub_2' => 'I\'m Creative I\'m Freelancer.I\'m Develop',
+    'main_slider_sub_3' => 'I\'m Creative I\'m Freelancer.I\'m Develop',
+    //------------------------------------------------------------------------------------------------------------------
+
+    'services_title_1' => 'ВЕБ ДИЗАЙН',
+    'services_title_2' => 'ВЕБ-РОЗРОБКА',
+    'services_title_3' => 'ВІДПОВІДАЛЬНИЙ ДИЗАЙН',
+    'services_title_sub_1' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    'services_title_sub_2' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    'services_title_sub_3' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    //------------------------------------------------------------------------------------------------------------------
+
+    'about_who' => 'ХТО МИ?',
+    'about_title' => 'VIVAMUS MAGNA JUSTO',
+    'about_body_1' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    'about_body_2' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.',
+    'about_skils_1' => 'Photoshop',
+    'about_skils_2' => 'Ilustrator',
+    'about_skils_3' => 'Web Design',
+    'about_skils_4' => 'Web Development',
+    //------------------------------------------------------------------------------------------------------------------
+
+    'counter_1' => 'Задоволені клієнти',
+    'counter_2' => 'Роки досвіду',
+    'counter_3' => 'Члени команди',
+    'counter_4' => 'Нагороди',
+    //------------------------------------------------------------------------------------------------------------------
+
+    'portfolio_title_1' => 'ОСТАННІ РОБОТИ',
+    'portfolio_title_3' => 'НАШЕ ПОРТФОЛІО',
+    'visitka' => 'Сайт візитка',
+    'landing_page' => 'Langing Page',
+    'e_commerce' => 'Інтернет магазин',
+    //------------------------------------------------------------------------------------------------------------------
+
+    'pricing_title_1' => 'ТАБЛИЦЯ ЦІН',
+    'from' => 'Від',
+    'description_visitka_1' => 'FULL ACCESS',
+    'description_visitka_2' => 'FULL ACCESS',
+    'description_visitka_3' => 'FULL ACCESS',
+    'description_visitka_4' => 'FULL ACCESS',
+    'description_landing_1' => 'FULL ACCESS',
+    'description_landing_2' => 'FULL ACCESS',
+    'description_landing_3' => 'FULL ACCESS',
+    'description_landing_4' => 'FULL ACCESS',
+    'description_e_commerce_1' => 'FULL ACCESS',
+    'description_e_commerce_2' => 'FULL ACCESS',
+    'description_e_commerce_3' => 'FULL ACCESS',
+    'description_e_commerce_4' => 'FULL ACCESS',
+    'here' => 'HIRE ME',
+    //------------------------------------------------------------------------------------------------------------------
+
+    'contact_title_1' => 'ПРИВІТАЙСЯ',
+    'contact_phone' => 'Телефон',
+    'contact_email' => 'E-mail',
+    'contact_address' => 'Адресса',
+    'contact_subject' => 'Тема',
+    'contact_message' => 'Повідомлення',
+    'contact_your_name' => 'Ваше ім\'я',
+    'contact_your_email' => 'Ваше E-mail',
+    'contact_button' => 'Відправити',
+
+
+
+];
