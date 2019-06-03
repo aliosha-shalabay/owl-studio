96acdc83fd9f427a1994c19d4e2165027ded9978
Aliosha <aliosha.shalabay@gmai.com>
2019-06-03 08:19:36 +0300

    google analitic

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -14,6 +14,16 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/all.css') }}" rel="stylesheet">
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141261771-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-141261771-1');
+    </script>
 </head>
 <body>
 
