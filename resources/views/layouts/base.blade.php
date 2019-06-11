6f02de6b4df1f547232d7347db367ae125c57970
Aliosha <aliosha.shalabay@gmai.com>
2019-06-11 11:38:54 +0300

    click analitik

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="{{$setting->seo_desc}}">
     <meta name="Keywords" content="{{$setting->seo_keywords}}">
-    <link rel="shortcut icon" type="image/x-icon" href="/images/icon.png" />
+    <link rel="shortcut icon" type="image/x-icon" href="/images/icon.png"/>
 
     <title>{{$setting->seo_title}}</title>
 
@@ -19,14 +19,39 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141261771-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
         gtag('js', new Date());
 
         gtag('config', 'UA-141261771-1');
     </script>
+
+    <!-- Google Tag Manager -->
+    <script>(function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-T5J2N4N');</script>
+    <!-- End Google Tag Manager -->
 </head>
 <body>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T5J2N4N"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
+<!-- End Google Tag Manager (noscript) -->
 <!--[if lt IE 8]>
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
     your browser</a> to improve your experience.</p>
